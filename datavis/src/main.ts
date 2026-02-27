cb1b6d0e975b6f5592b8c08f7c661943b3615f67
josephniet <josephniet@gmail.com>
2026-02-27 16:29:37 +0000

    added basic grid

--- datavis/src/main.ts
+++ datavis/src/main.ts
@@ -1,2 +1,12 @@
 import './style.css'
-import { scoreData } from './store/scoreData.ts';
+import { GridVisualiser } from './components/GridVisauliser.ts';
+
+//external app stuff
+const level1Button = document.getElementById('level-one') as HTMLButtonElement;
+const level2Button = document.getElementById('level-two') as HTMLButtonElement;
+const level3Button = document.getElementById('level-three') as HTMLButtonElement;
+const level4Button = document.getElementById('level-four') as HTMLButtonElement;
+const level5Button = document.getElementById('level-five') as HTMLButtonElement;
+const circleGraphButton = document.getElementById('circle-graph') as HTMLButtonElement;
+
+
